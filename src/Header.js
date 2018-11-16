6b8c6db86878a95a64db58a126e267a089e7b669
Stephane Moreau <st.moreau@outlook.com.gr>
2018-11-16 20:46:03 +0000

    gh-pages

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import logo from "./img/logo.png";
 const Header = () => (
   <nav>
     <div className="nav-wrapper container">
-      <Link to="/" className="brand-logo left">
+      <Link to={process.env.PUBLIC_URL + "/"} className="brand-logo left">
         <img style={{ width: "70px" }} src={logo} alt="logo" />
       </Link>
       <ul className="right">
