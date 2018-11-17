6cd43bb4806a6d67e6492872cef066f9afcd0a8d
Stephane Moreau <st.moreau@outlook.com.gr>
2018-11-17 15:17:13 +0000

    eject & add less

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import "./css/index.css";
+import "./less/index.less";
 
 render(
   <BrowserRouter>
