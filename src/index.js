756edb6228c64ee0e87ad453e64f686812ab91af
Stephane Moreau <st.moreau@outlook.com.gr>
2018-11-24 20:16:38 +0000

    add GA

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from 'react-ga';
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./less/index.less";
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 render(
   <BrowserRouter>
@@ -12,3 +15,6 @@ render(
   document.getElementById("root")
 );
 registerServiceWorker();
+
+ReactGA.initialize('UA-129691022-01');
+ReactGA.pageview(window.location.pathname + window.location.search);
